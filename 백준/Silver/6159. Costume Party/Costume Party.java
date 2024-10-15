4ae7c2727bc60df7dd6f83b89ccb5f27ff551a77
junsu <junsu120202@gmail.com>
2024-10-15 16:11:49 +0900

    [Silver V] Title: Costume Party, Time: 124 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Silver/6159. Costume Party/Costume Party.java
+++ 백준/Silver/6159. Costume Party/Costume Party.java
@@ -5,35 +5,36 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int n = Integer.parseInt(st.nextToken());
-		int l = Integer.parseInt(st.nextToken());
-		
-		int arr[] = new int[n];
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(arr);
-		
-		int cnt = 0, left = 0, right = 1;
-		for(int i = 0; i < n-1; i++) {
-			for(int j = i+1; j < n; j++) {
-				if(arr[i]+arr[j] > l) {
-					break;
-				}
-				
-				cnt++;
-			}
-		}
-		
-		System.out.println(cnt);
-		
-		br.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            if (arr[left] + arr[right] <= l) {
+                cnt += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(cnt);
+
+        br.close();
+    }
 
 }
