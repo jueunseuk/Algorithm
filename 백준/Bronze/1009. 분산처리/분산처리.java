59e2fab7cc25c3689c257f8522ece6495c0eac82
junsu <junsu120202@gmail.com>
2024-11-27 22:51:26 +0900

    [Bronze II] Title: 분산처리, Time: 68 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -4,78 +4,80 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int down = Integer.parseInt(st.nextToken())%10;
-			int up = Integer.parseInt(st.nextToken());
-			
-			if(down == 1) {
-				System.out.println(1);
-			} else if( down == 2) {
-				if(up % 4 == 1) {
-					System.out.println(2);
-				} else if(up % 4 == 2) {
-					System.out.println(4);
-				} else if(up % 4 == 3) {
-					System.out.println(8);
-				} else {
-					System.out.println(6);
-				}
-			} else if( down == 3) {
-				if(up % 4 == 1) {
-					System.out.println(3);
-				} else if(up % 4 == 2) {
-					System.out.println(9);
-				} else if(up % 4 == 3) {
-					System.out.println(7);
-				} else {
-					System.out.println(1);
-				}
-			} else if( down == 4) {
-				if(up % 2 == 1) {
-					System.out.println(4);
-				} else {
-					System.out.println(6);
-				}
-			} else if( down == 5) {
-				System.out.println(5);
-			} else if( down == 6) {
-				System.out.println(6);
-			} else if( down == 7) {
-				if(up % 4 == 1) {
-					System.out.println(7);
-				} else if(up % 4 == 2) {
-					System.out.println(9);
-				} else if(up % 4 == 3) {
-					System.out.println(3);
-				} else {
-					System.out.println(1);
-				}
-			} else if( down == 8) {
-				if(up % 4 == 1) {
-					System.out.println(8);
-				} else if(up % 4 == 2) {
-					System.out.println(4);
-				} else if(up % 4 == 3) {
-					System.out.println(2);
-				} else {
-					System.out.println(6);
-				}
-			} else if( down == 9) {
-				if(up % 2 == 1) {
-					System.out.println(9);
-				} else {
-					System.out.println(1);
-				}
-			} else {
-				System.out.println(10);
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int down = Integer.parseInt(st.nextToken()) % 10;
+            int up = Integer.parseInt(st.nextToken());
+
+            if (down == 1) {
+                sb.append(1).append("\n");
+            } else if (down == 2) {
+                if (up % 4 == 1) {
+                    sb.append(2).append("\n");
+                } else if (up % 4 == 2) {
+                    sb.append(4).append("\n");
+                } else if (up % 4 == 3) {
+                    sb.append(8).append("\n");
+                } else {
+                    sb.append(6).append("\n");
+                }
+            } else if (down == 3) {
+                if (up % 4 == 1) {
+                    sb.append(3).append("\n");
+                } else if (up % 4 == 2) {
+                    sb.append(9).append("\n");
+                } else if (up % 4 == 3) {
+                    sb.append(7).append("\n");
+                } else {
+                    sb.append(1).append("\n");
+                }
+            } else if (down == 4) {
+                if (up % 2 == 1) {
+                    sb.append(4).append("\n");
+                } else {
+                    sb.append(6).append("\n");
+                }
+            } else if (down == 5) {
+                sb.append(5).append("\n");
+            } else if (down == 6) {
+                sb.append(6).append("\n");
+            } else if (down == 7) {
+                if (up % 4 == 1) {
+                    sb.append(7).append("\n");
+                } else if (up % 4 == 2) {
+                    sb.append(9).append("\n");
+                } else if (up % 4 == 3) {
+                    sb.append(3).append("\n");
+                } else {
+                    sb.append(1).append("\n");
+                }
+            } else if (down == 8) {
+                if (up % 4 == 1) {
+                    sb.append(8).append("\n");
+                } else if (up % 4 == 2) {
+                    sb.append(4).append("\n");
+                } else if (up % 4 == 3) {
+                    sb.append(2).append("\n");
+                } else {
+                    sb.append(6).append("\n");
+                }
+            } else if (down == 9) {
+                if (up % 2 == 1) {
+                    sb.append(9).append("\n");
+                } else {
+                    sb.append(1).append("\n");
+                }
+            } else {
+                sb.append(10).append("\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
 }
