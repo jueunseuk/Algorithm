8fa406f5d8517901bfe5dc4ca8480e0a4dc19a51
junsu <junsu120202@gmail.com>
2025-01-26 11:02:24 +0900

    [Gold V] Title: Bee Problem, Time: 716 ms, Memory: 118420 KB -BaekjoonHub

--- 백준/Gold/16294. Bee Problem/Bee Problem.java
+++ 백준/Gold/16294. Bee Problem/Bee Problem.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int row, col, k;
+    static int row, col, k;
     static String[][] matrix;
     static boolean[][] visit;
     
     static int[] dx = {1, 1, -1, -1, 0, 0};
     static int[] dy = {1, -1, 1, -1, 2, -2};
-    
-    static StringBuilder result = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,30 +22,30 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         k = Integer.parseInt(st.nextToken());
         row = Integer.parseInt(st.nextToken());
-        col = Integer.parseInt(st.nextToken())*2;
+        col = Integer.parseInt(st.nextToken()) * 2;
         
         matrix = new String[row][col];
         visit = new boolean[row][col];
         
         for(int i = 0; i < row; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-        	if(i % 2 == 0) {
-        		for(int j = 0; j < col; j++) {
-        			if(j % 2 == 0) {
-        				matrix[i][j] = st.nextToken();
-        			} else {
-        				matrix[i][j] = " ";
-        			}
-        		}
-        	} else {
-        		for(int j = 0; j < col; j++) {
-        			if(j % 2 == 1) {
-        				matrix[i][j] = st.nextToken();
-        			} else {
-        				matrix[i][j] = " ";
-        			}
-        		}
-        	}
+            st = new StringTokenizer(br.readLine(), " ");
+            if(i % 2 == 0) {
+                for(int j = 0; j < col; j++) {
+                    if(j % 2 == 0) {
+                        matrix[i][j] = st.nextToken();
+                    } else {
+                        matrix[i][j] = " ";
+                    }
+                }
+            } else {
+                for(int j = 0; j < col; j++) {
+                    if(j % 2 == 1) {
+                        matrix[i][j] = st.nextToken();
+                    } else {
+                        matrix[i][j] = " ";
+                    }
+                }
+            }
         }
         
         if (k == 0) {
@@ -53,44 +53,59 @@ public class Main {
             return;
         }
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> compSizes = new ArrayList<>();
+        
         for(int i = 0; i < row; i++) {
-        	for(int j = 0; j < col; j++) {
-    			if(!visit[i][j] && matrix[i][j].equals(".")) {
-    				list.add(dfs(i, j));
-    			}
-        	}
+            for(int j = 0; j < col; j++) {
+                if(!visit[i][j] && ".".equals(matrix[i][j])) {
+                    compSizes.add(bfs(i, j));
+                }
+            }
         }
         
-        Collections.sort(list, Collections.reverseOrder());
+        Collections.sort(compSizes, Collections.reverseOrder());
         
         int sum = 0;
-        int i;
-        for(i = 0; i < list.size(); i++) {
-        	sum += list.get(i);
-        	
-        	if(sum >= k) {
-        		break;
-        	}
+        int idx;
+        for(idx = 0; idx < compSizes.size(); idx++) {
+            sum += compSizes.get(idx);
+            if(sum >= k) {
+                break;
+            }
         }
         
-        System.out.println(i+1);
+        System.out.println(idx + 1);
+    }
+    
+    static int bfs(int si, int sj) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        visit[si][sj] = true;
+        queue.offer(new int[]{si, sj});
         
-	}
+        int count = 0;
+        
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            count++;
+            
+            int cx = cur[0];
+            int cy = cur[1];
+            
+            for(int d = 0; d < 6; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                
+                if(nx < 0 || nx >= row || ny < 0 || ny >= col) {
+                    continue;
+                }
+                if(!visit[nx][ny] && ".".equals(matrix[nx][ny])) {
+                    visit[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
+        
+        return count;
+    }
 
-	public static int dfs(int i, int j) {
-		visit[i][j] = true;
-		int depth = 1;
-		
-		for(int delta = 0; delta < 6; delta++) {
-			int nx = i + dx[delta];
-			int ny = j + dy[delta];
-			
-			if(nx >= 0 && ny >= 0 && nx < row && ny < col && !visit[nx][ny] && matrix[nx][ny].equals(".")) {
-				depth += dfs(nx, ny);
-			}
-		}
-		
-		return depth;
-	}
 }
