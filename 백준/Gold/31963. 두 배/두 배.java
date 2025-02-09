105e2a982e79db24400fb9f0482669c8d678d377
junsu <junsu120202@gmail.com>
2025-02-09 20:31:23 +0900

    [Gold IV] Title: 두 배, Time: 280 ms, Memory: 42068 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -3,27 +3,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        
-        long[] arr = new long[n];
-        
+        double[] logValues = new double[n];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        int cnt = 0;
-        arr[0] = Long.parseLong(st.nextToken());
-        for(int i = 1; i < n; i++) {
-        	arr[i] = Long.parseLong(st.nextToken());
-        	while(arr[i] < arr[i-1]) {
-        		arr[i] *= 2;
-        		cnt++;
-        	}
-        }
-        
-        System.out.println(cnt);
-	}
 
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            logValues[i] = Math.log(value) / Math.log(2);
+        }
+
+        long cnt = 0;
+        final double EPS = 1e-10;
+
+        for (int i = 1; i < n; i++) {
+            if (logValues[i] + EPS < logValues[i - 1]) {
+                int k = (int) Math.ceil(logValues[i - 1] - logValues[i] - EPS);
+                cnt += k;
+                logValues[i] += k;
+            }
+        }
+
+        System.out.println(cnt);
+    }
 }
