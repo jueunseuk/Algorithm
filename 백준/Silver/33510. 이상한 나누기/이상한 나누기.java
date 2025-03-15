2cbc53df0e109f0b412ac74338ed216b5c33d124
junsu <junsu120202@gmail.com>
2025-03-15 13:31:42 +0900

    [Silver II] Title: 이상한 나누기, Time: 360 ms, Memory: 119072 KB -BaekjoonHub

--- 백준/Silver/33510. 이상한 나누기/이상한 나누기.java
+++ 백준/Silver/33510. 이상한 나누기/이상한 나누기.java
@@ -1,45 +1,61 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int length = Integer.parseInt(br.readLine());
+        String input = br.readLine();
         
-        char[]input = br.readLine().toCharArray();
-        
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i = length-1; i >= 0; i--) {
-        	q.offer(input[i]-'0');
+        char[] bits = new char[length + 2];
+        for (int i = 0; i < bits.length; i++) {
+            bits[i] = '0';
         }
         
-        int oddCnt = 0;
-        while(q.size() > 1) {
-        	while(q.size() > 1 && q.peekFirst() == 0) {
-        		q.pollFirst();
-        	}
-        	
-        	if(q.size() > 1 && q.peekFirst() == 1) {
-        		q.pollFirst();
-        		oddCnt++;
-        		
-        		while(q.size() > 0 && q.peekFirst() == 1) {
-        			q.pollFirst();
-        		}
-        		
-        		if(q.size() == 0) {
-        			q.offerFirst(1);
-        		} else {
-        			q.pollFirst();
-        			q.offerFirst(1);
-        		}
-        	}
+        for (int i = 0; i < length; i++) {
+            bits[i] = input.charAt(length - 1 - i);
         }
-        
-        System.out.println(oddCnt);
-	}
+
+        int end = length - 1;
+
+        int start = 0;
+        int oddCount = 0;
+
+        while (true) {
+            while (start <= end && bits[start] == '0') {
+                start++;
+            }
+
+            if (start > end) {
+                System.out.println(oddCount);
+                return;
+            }
+            if (start == end && bits[end] == '1') {
+                System.out.println(oddCount);
+                return;
+            }
+
+            if (bits[start] == '0') {
+                start++;
+            } else {
+                oddCount++;
+
+                int idx = start;
+                while (idx <= end && bits[idx] == '1') {
+                    bits[idx] = '0';
+                    idx++;
+                }
+                
+                if (idx <= end) {
+                    bits[idx] = '1';
+                } else {
+                    end++;
+                    bits[end] = '1';
+                }
+                
+                start++;
+            }
+        }
+    }
 }
