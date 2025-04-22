ff4d16e1fa1de6c05bb9a2346c6eaac2359fdfc5
junsu <junsu120202@gmail.com>
2025-04-22 12:14:41 +0900

    [Silver IV] Title: 상근이의 여행, Time: 144 ms, Memory: 22948 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,16 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m, cnt;
-	static List<List<Integer>> list;
-	static boolean visit[];
+	static int n, m;
 
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,51 +17,14 @@ public class Main {
         	st = new StringTokenizer(br.readLine(), " ");
         	n = Integer.parseInt(st.nextToken());
         	m = Integer.parseInt(st.nextToken());
-        	cnt = 0;
 
-        	visit = new boolean[n];
-        	list = new ArrayList<>();
-        	for(int i = 0; i < n; i++) {
-        		list.add(new ArrayList<>());
-        	}
-        	
         	for(int i = 0; i < m; i++) {
-        		st = new StringTokenizer(br.readLine(), " ");
-        		int start = Integer.parseInt(st.nextToken())-1;
-        		int end = Integer.parseInt(st.nextToken())-1;
-        		
-        		list.get(start).add(end);
-        		list.get(end).add(start);
+        		br.readLine();
         	}
         	
-        	for(int i = 0; i < n; i++) {
-        		if(!visit[i]) {
-        			bfs(i);
-        		}
-        	}
-        	
-        	sb.append(cnt+"\n");
+        	sb.append(n-1+"\n");
         }
         
         System.out.println(sb.toString().trim());
-	}
-
-	private static void bfs(int i) {
-		Queue<Integer> q = new LinkedList<>();
-		
-		visit[i] = true;
-		q.offer(i);
-		
-		while(!q.isEmpty()) {
-			int poll = q.poll();
-			
-			for(Integer out : list.get(poll)) {
-				if(!visit[out]) {
-					q.offer(out);
-					visit[out] = true;
-					cnt++;
-				}
-			}
-		}
 	}
 }
