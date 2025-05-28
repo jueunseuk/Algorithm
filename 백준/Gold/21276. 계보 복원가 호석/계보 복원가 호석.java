03bab8446488dd71ca93d138036037fe7608fb43
junsu <junsu120202@gmail.com>
2025-05-28 14:34:04 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 820 ms, Memory: 151512 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static Map<String, Integer> id = new HashMap<>();
+    static String[] name;
+    static List<List<Integer>> g = new ArrayList<>();
+    static int[] indeg;
+    static List<List<Integer>> child;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        name = new String[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            name[i] = st.nextToken();
+        }
+        Arrays.sort(name);
+        for (int i = 0; i < N; i++) id.put(name[i], i);
+
+        M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            g.add(new ArrayList<>());
+        }
+        indeg = new int[N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int childIdx   = id.get(st.nextToken());
+            int parentIdx  = id.get(st.nextToken());
+            g.get(parentIdx).add(childIdx);
+            indeg[childIdx]++;
+        }
+
+        child = new ArrayList<>();
+        for (int i = 0; i < N; i++) child.add(new ArrayList<>());
+
+        topoAndBuild();
+
+        List<String> roots = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+        	if (indeg[i] == 0) roots.add(name[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(roots.size()).append('\n');
+        for (String r : roots) {
+        	sb.append(r).append(' ');
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append('\n');
+
+        for (int i = 0; i < N; i++) {
+            List<Integer> cList = child.get(i);
+            Collections.sort(cList);
+            
+            sb.append(name[i]).append(' ').append(cList.size()).append(' ');
+            if(cList.size() == 0) {
+            	sb.deleteCharAt(sb.length()-1);
+            }
+            cList.stream().map(idx -> name[idx]).forEach(s -> sb.append(s));
+            sb.append('\n');
+        }
+        
+        System.out.print(sb.toString());
+    }
+
+    private static void topoAndBuild() {
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) if (indeg[i] == 0) q.add(i);
+
+        int[] indegCopy = indeg.clone();
+        while (!q.isEmpty()) {
+            int p = q.poll();
+            for (int c : g.get(p)) {
+                if (--indeg[c] == 0) {
+                    child.get(p).add(c);
+                    q.add(c);
+                }
+            }
+        }
+        indeg = indegCopy;
+    }
+}
