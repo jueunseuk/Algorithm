ad5ae64efe98d18abcf686b6f1c9c600e42058eb
junsu <junsu120202@gmail.com>
2025-06-27 19:30:49 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 84 ms, Memory: 11744 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        long n = Long.parseLong(br.readLine());
+        long result = n;
+        
+        int limit = (int) Math.sqrt(n);
+        for(int i = 2; i <= limit; i++) {
+        	if(n % i == 0) {
+        		while(n % i == 0) {
+        			n /= i;
+        		}
+        		
+        		result -= result/i;
+        	}
+        	
+        }
+        if(n > 1) {
+        	result -= result/n;
+        }
+        
+        System.out.println(result);
+	}
+}
