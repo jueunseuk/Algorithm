0beac8aa85ed9a5f156a545c81b9832920f2a034
junsu <junsu120202@gmail.com>
2025-06-27 18:58:18 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 512 ms, Memory: 60548 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static boolean[] prime = new boolean[1000001];
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        preprocess();
+        
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+        	int input = Integer.parseInt(br.readLine());
+        	
+        	if(input == 0) break;
+        	
+        	int a = 2;
+        	int b = input-2;
+        	
+        	while(a <= b) {
+        		if(prime[a] && prime[b]) {
+        			sb.append(input+" = "+a+" + "+b+"\n");
+        			break;
+        		}
+        		a++; b--;
+        	}
+        }
+
+        System.out.println(sb.toString().trim());
+	}
+
+	private static void preprocess() {
+		Arrays.fill(prime, true);
+		
+		prime[0] = false;
+		prime[1] = false;
+		
+		for(int i = 2; i < Math.sqrt(1000001); i++) {
+			for(int j = i*i; j < 1000001; j += i) {
+				if(prime[j]) {
+					prime[j] = false;
+				}
+			}
+		}
+	}
+}
