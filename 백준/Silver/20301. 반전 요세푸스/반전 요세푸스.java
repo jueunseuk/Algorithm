9f69e7c60ecfa8dd4ee279b7621e84e5bf871f4f
junsu <junsu120202@gmail.com>
2025-06-30 12:20:34 +0900

    [Silver III] Title: 반전 요세푸스, Time: 468 ms, Memory: 23972 KB -BaekjoonHub

---
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = 1; i <= n; i++) {
+        	dq.offerLast(i);
+        }
+        
+        boolean dir = true;
+        StringBuilder sb = new StringBuilder();
+        while(!dq.isEmpty()) {
+        	if(dir) {
+        		for(int i = 0; i < k-1; i++) {
+        			dq.offerLast(dq.pollFirst());
+        		}
+        		sb.append(dq.pollFirst()+"\n");
+        	} else {
+        		for(int i = 0; i < k-1; i++) {
+        			dq.offerFirst(dq.pollLast());
+        		}
+        		sb.append(dq.pollLast()+"\n");
+        	}
+        	
+        	if((n-dq.size()) % m == 0) {
+        		dir = !dir;
+        	}
+        }
+        
+        System.out.println(sb.toString().trim());
+	}
+}
