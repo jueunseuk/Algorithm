05863928dc6a0305a235a95ea20a3b012ffd9c27
junsu <junsu120202@gmail.com>
2025-07-10 12:14:23 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        if(n == 1 && m == 1) {
+        	System.out.println(0);
+        } else if(n == 1) {
+        	System.out.println(m-1);
+        } else if(m == 1) {
+        	System.out.println(n-1);
+        } else {
+        	System.out.println(n-1+(n*(m-1)));
+        }
+	}
+}
