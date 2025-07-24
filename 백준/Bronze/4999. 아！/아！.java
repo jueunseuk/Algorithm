e05f409d7d2a7b9d40dd0fc254e3f023f9254e8a
junsu <junsu120202@gmail.com>
2025-07-24 18:06:48 +0900

    [Bronze V] Title: 아!, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		
+		if(str1.length() < str2.length()) {
+			System.out.println("no");
+		} else {
+			System.out.println("go");
+		}
+	}
+}
