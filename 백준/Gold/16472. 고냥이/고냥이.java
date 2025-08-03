8d11fd520004d0501a9f3e5be77e839389cc9591
junsu <junsu120202@gmail.com>
2025-08-03 12:21:30 +0900

    [Gold IV] Title: 고냥이, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int limit = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int n = str.length();
+
+        if (limit >= 26) {
+            System.out.println(n);
+            return;
+        }
+
+        int[] cnt = new int[26];
+        int kinds = 0;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            int rc = str.charAt(right) - 'a';
+            if(cnt[rc]++ == 0) kinds++;
+
+            while(kinds > limit) {
+                int lc = str.charAt(left) - 'a';
+                if (--cnt[lc] == 0) kinds--;
+                left++;
+            }
+            
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        System.out.println(ans);
+    }
+}
