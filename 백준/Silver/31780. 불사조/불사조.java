3da1650b43ec98969eb8886d94a670fefda56855
junsu <junsu120202@gmail.com>
2025-08-03 22:02:07 +0900

    [Silver V] Title: 불사조, Time: 64 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/31780. 불사조/불사조.java
+++ 백준/Silver/31780. 불사조/불사조.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,32 +12,6 @@ public class Main {
         int x = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(x);
-        
-        int sum = x;
-        for(int i = 0; i < m; i++) {
-        	int size = q.size();
-        	
-        	for(int j = 0; j < size; j++) {
-        		int poll = q.poll();
-        		int first = 0;
-        		int second = 0;
-        		
-        		if(poll % 2 == 0) {
-        			first = second = poll/2;
-        		} else {
-        			first = poll/2;
-        			second = first+1;
-        		}
-        		
-        		q.offer(first);
-        		q.offer(second);
-        		
-        		sum += first+second;
-        	}
-        }
-
-        System.out.println(sum);
+        System.out.println(x*(m+1));
 	}
 }
