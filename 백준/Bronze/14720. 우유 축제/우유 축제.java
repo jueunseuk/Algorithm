51c83408cc86e02ddc913cda10124601376bb15e
junsu <junsu120202@gmail.com>
2025-09-06 09:08:15 +0900

    [Bronze II] Title: 우유 축제, Time: 72 ms, Memory: 11780 KB -BaekjoonHub

---
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int input[] = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++) {
+        	input[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int cnt = 0;
+        int target = 0;
+        for(int out : input) {
+        	if(out == target) {
+        		cnt++;
+        		target++;
+        		target %= 3;
+        	}
+        }
+        
+        System.out.println(cnt);
+	}
+}
