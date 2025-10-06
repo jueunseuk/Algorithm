1635e96d324ec02b090ed96d035e34dae840062e
junsu <junsu120202@gmail.com>
2025-10-06 09:07:13 +0900

    [Bronze III] Title: 지능형 기차, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int result = 0;
+        int curr = 0;
+        
+        StringTokenizer st;
+        for(int i = 0; i < 4; i++) {
+        	st = new StringTokenizer(br.readLine()," ");
+        	curr -= Integer.parseInt(st.nextToken());
+        	curr += Integer.parseInt(st.nextToken());
+        	
+        	result = Math.max(result, curr);
+        }
+        
+        System.out.println(result);
+	}
+}
