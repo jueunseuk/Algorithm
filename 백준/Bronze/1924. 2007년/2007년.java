495bab0d3193978c082b082f0c3a27d1b15923b4
junsu <junsu120202@gmail.com>
2025-10-10 20:29:55 +0900

    [Bronze I] Title: 2007년, Time: 208 ms, Memory: 19516 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String month = st.nextToken();
+        String day = st.nextToken();
+        
+        if(month.length() == 1) {
+        	month = "0"+month;
+        }
+        if(day.length() == 1) {
+        	day = "0"+day;
+        }
+        
+        LocalDate target = LocalDate.parse(String.valueOf("2007-"+month+"-"+day));
+        System.out.println(target.getDayOfWeek().toString().substring(0, 3));
+	}
+}
