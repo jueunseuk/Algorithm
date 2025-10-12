abff398d1d3317065b9ce9fbd6e8e9dd2ff023e9
junsu <junsu120202@gmail.com>
2025-10-12 09:50:26 +0900

    [Silver IV] Title: 2+1 세일, Time: 312 ms, Memory: 26296 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        long result = 0;
+        
+        Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < n; i++) {
+        	q.offer(Integer.parseInt(br.readLine()));
+        }
+
+        while(q.size() >= 3) {
+        	result += q.poll();
+        	result += q.poll();
+        	q.poll();
+        }
+        
+        while(!q.isEmpty()) {
+        	result += q.poll();
+        }
+        
+        System.out.println(result);
+	}
+}
