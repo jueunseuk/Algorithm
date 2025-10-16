5e94983d79407733dce2665694cd61c5b5e524ce
junsu <junsu120202@gmail.com>
2025-10-16 22:45:56 +0900

    [Bronze I] Title: 캠핑, Time: 76 ms, Memory: 12020 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
+        while(true) {
+        	st = new StringTokenizer(br.readLine(), " ");
+        	long l = Integer.parseInt(st.nextToken());
+        	long p = Integer.parseInt(st.nextToken());
+        	long v = Integer.parseInt(st.nextToken());
+        	
+        	if(l == 0) {
+        		break;
+        	}
+        	
+        	sb.append("Case ").append(cnt++).append(": ").append(l * (v / p) + Math.min(l, v % p)).append("\n");
+        }
+
+        System.out.println(sb.toString().trim());
+	}
+}
