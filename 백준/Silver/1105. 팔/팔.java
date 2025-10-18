ec77e6448a4192dec0c4512b279b73c00d776353
junsu <junsu120202@gmail.com>
2025-10-18 09:22:05 +0900

    [Silver I] Title: 팔, Time: 60 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String l = st.nextToken();
+        String r = st.nextToken();
+        
+        int cnt = 0;
+        if(l.length() == r.length()) {
+        	if(l.equals(r)) {
+        		for(int i = 0; i < l.length(); i++) {
+        			if(l.charAt(i) == '8') cnt++;
+        		}
+        		System.out.println(cnt);
+        	} else {
+        		int fix = 0;
+        		for(int i = 0; i < l.length(); i++) {
+        			if(l.charAt(i) == r.charAt(i)) {
+        				if(l.charAt(i) == '8') cnt++;
+        			} else {
+        				break;
+        			}
+        		}
+        		System.out.println(cnt);
+        	}
+        } else {
+        	System.out.println(0);
+        }
+	}
+}
