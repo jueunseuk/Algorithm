892613360499edeb4dd371fd0926fe4bec3e83de
junsu <junsu120202@gmail.com>
2025-10-21 22:58:51 +0900

    [Bronze II] Title: TV 크기, Time: 68 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/1297. TV 크기/TV 크기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine() ," ");
+        double diagonal = Double.parseDouble(st.nextToken());
+        double w = Double.parseDouble(st.nextToken());
+        double h = Double.parseDouble(st.nextToken());
+        
+        double d = Math.sqrt(w*w+h*h);
+        double div = diagonal / d;
+        
+        System.out.println((int)Math.floor(w*div)+" "+(int)Math.floor(h*div));
+	}
+}
