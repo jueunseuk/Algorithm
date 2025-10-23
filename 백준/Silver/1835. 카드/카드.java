fda44a94d3e2066d420385e34cd0dd3b93d638ec
junsu <junsu120202@gmail.com>
2025-10-23 17:07:47 +0900

    [Silver IV] Title: 카드, Time: 96 ms, Memory: 12500 KB -BaekjoonHub

---
+++ 백준/Silver/1835. 카드/카드.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        Deque<Integer> deq = new ArrayDeque<>();
+                
+        for (int i = n; i >= 1; i--) {
+            deq.offerFirst(i);
+            for (int k = 0; k < i; k++) {
+                deq.offerFirst(deq.pollLast());
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int size = deq.size();
+        for(int i = 0; i < size; i++) {
+        	sb.append(deq.pollFirst()+" ");
+        }
+        
+        System.out.println(sb.toString().trim());
+	}
+}
