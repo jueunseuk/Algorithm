dd9492d8834df6c3ab3dcc6642bf548baff8348a
junsu <junsu120202@gmail.com>
2025-10-27 21:40:31 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[] input = new int[11];
+        for(int i = 1; i <= 10; i++) {
+        	input[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int i = 1; i <= 10; i++) {
+        	input[i] += input[i-1];
+        }
+        
+        int[] gap = new int[11];
+        for(int i = 1; i <= 10; i++) {
+        	gap[i] = Math.abs(100-input[i]);
+        }
+        
+        int idx = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 10; i >= 1; i--) {
+        	if(gap[i] < min) {
+        		min = gap[i];
+        		idx = i;
+        	} else break;
+        }
+        
+        System.out.println(input[idx]);
+	}
+}
