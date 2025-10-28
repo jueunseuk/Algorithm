14c12603213e3952d01b904390d3dc1f7ca25140
junsu <junsu120202@gmail.com>
2025-10-28 14:08:40 +0900

    [Bronze V] Title: 카드 게임, Time: 64 ms, Memory: 11428 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int j = 0;
+        for(int i = 0; i < 5; i++)
+        {
+        	j += Integer.parseInt(br.readLine());
+        }
+        
+        System.out.println(j);
+	}
+}
