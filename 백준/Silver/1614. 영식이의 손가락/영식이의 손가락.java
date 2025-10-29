12d25f70fe530dafa9989cc4b19763e10885e942
junsu <junsu120202@gmail.com>
2025-10-29 13:55:42 +0900

    [Silver III] Title: 영식이의 손가락, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static final long CYCLE = 8;
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int hurt = Integer.parseInt(br.readLine());
+        long count = Integer.parseInt(br.readLine());
+        
+        long result = 0;
+        switch(hurt) {
+	        case 1: {
+        		result = CYCLE * count;
+	        	break;
+	        }
+	        case 2: {
+	        	if(count % 2 == 0) {
+	        		result = CYCLE * count / 2 + 1;
+	        	} else {
+	        		result = CYCLE * (count-1) / 2 + 7;
+	        	}
+	        	break;
+	        }
+	        case 3: {
+	        	if(count % 2 == 0) {
+	        		result = CYCLE * count / 2 + 2;
+	        	} else {
+	        		result = CYCLE * (count-1) / 2 + 6;
+	        	}
+	        	break;
+	        }
+	        case 4: {
+	        	if(count % 2 == 0) {
+	        		result = CYCLE * count / 2 + 3;
+	        	} else {
+	        		result = CYCLE * (count-1) / 2 + 5;
+	        	}
+	        	break;
+	        }
+	        case 5: {
+	        	result = CYCLE * count + 4;
+	        	break;
+	        }
+        }
+
+        System.out.println(result);
+	}
+}
