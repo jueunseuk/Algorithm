fedf3f1ceb9c7fa71d0e7274634b5edc70bcd9ee
junsu <junsu120202@gmail.com>
2026-01-25 10:15:54 +0900

    [Silver IV] Title: 삼삼한 수, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/17252. 삼삼한 수/삼삼한 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        if(n == 0) {
+        	System.out.println("NO");
+        	return;
+        }
+        
+    	for(int i = 19; i >= 0; i--) {
+    		int pow = (int) Math.pow(3, i);
+    		if(pow <= n) {
+    			n -= pow;
+    		}
+    	}
+        
+        System.out.println(n == 0 ? "YES" : "NO");
+	}
+}
