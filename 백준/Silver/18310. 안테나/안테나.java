ff60e04909de791c4cb40f527620cb57ae143d10
junsu <junsu120202@gmail.com>
2026-02-22 12:36:11 +0900

    [Silver III] Title: 안테나, Time: 260 ms, Memory: 38304 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] input = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	input[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(input);
+        
+        System.out.println(input[(n-1)/2]);
+	}
+}
