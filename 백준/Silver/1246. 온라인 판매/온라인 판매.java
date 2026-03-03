e2917e1b87d9a9436ff4d51bacfa362ee2938683
junsu <junsu120202@gmail.com>
2026-03-03 18:05:12 +0900

    [Silver IV] Title: 온라인 판매, Time: 72 ms, Memory: 11932 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < m; i++) {
+        	pq.offer(Integer.parseInt(br.readLine()));
+        }
+        
+        int price = 0;
+        int max = 0;
+        while(!pq.isEmpty()) {
+        	if(max < (m-pq.size()+1) * pq.peek()) {
+        		max = (m-pq.size()+1) * pq.peek();
+        		price = pq.peek();
+        	}
+        	pq.poll();
+        	if(--n == 0) break;
+        }
+        
+        System.out.println(price+" "+max);
+	}
+}
