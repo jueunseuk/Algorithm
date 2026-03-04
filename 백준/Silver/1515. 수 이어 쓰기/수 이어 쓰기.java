b43e93b2cfb342238cfebdde77872d31ab967275
junsu <junsu120202@gmail.com>
2026-03-04 15:43:33 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 92 ms, Memory: 13828 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+
+        int idx = 0;
+        for(int i = 1; ; i++) {
+            String target = String.valueOf(i);
+            for(int k = 0; k < target.length(); k++) {
+                if(idx < s.length() && target.charAt(k) == s.charAt(idx)) {
+                    idx++;
+                    if(idx == s.length()) {
+                        System.out.println(i);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
