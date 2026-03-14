f134eb72df5cd7cc364f701cf03422fb7908d031
junsu <junsu120202@gmail.com>
2026-03-14 09:13:00 +0900

    [Silver IV] Title: donstructive, Time: 256 ms, Memory: 44596 KB -BaekjoonHub

---
+++ 백준/Silver/30618. donstructive/donstructive.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		for(int i = n; i >= 1; i--) {
+			if(i % 2 == 0) {
+				dq.offerFirst(i);
+			} else {
+				dq.offerLast(i);
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(!dq.isEmpty()) {
+			sb.append(dq.pollFirst()).append(' ');
+		}
+		
+		System.out.println(sb.toString().trim());
+	}
+}
