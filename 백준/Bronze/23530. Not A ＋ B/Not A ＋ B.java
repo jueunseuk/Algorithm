4a7b3b5eaed7adbad97b305f4c699653d2eebb73
junsu <junsu120202@gmail.com>
2026-03-17 12:28:05 +0900

    [Bronze III] Title: Not A + B, Time: 72 ms, Memory: 11996 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < t; i++) {
+        		st = new StringTokenizer(br.readLine(), " ");
+        		int sum = (Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+1)%50;
+        		if(sum == 0) sum += 1;
+        		sb.append(sum).append("\n");
+        }
+        
+        System.out.println(sb.toString().trim());
+	}
+}
