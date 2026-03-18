8d18e21dd8f9c6b85a639dd1331bc0c37318864a
junsu <junsu120202@gmail.com>
2026-03-18 10:17:24 +0900

    [Bronze IV] Title: 2, 4, 6, 8, Time: 60 ms, Memory: 11164 KB -BaekjoonHub

---
+++ 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+        for(int i = 1; i <= 9; i++) {
+        	for(int j = 0; j <= 9; j++) {
+        		if((j*10+i) % 4 == 0) {
+        			if((i + j) % 6 == 0) {
+        				if(i != 8 && j != 8) {
+        					System.out.println(i*10+j);
+        					return;
+        				}
+        			}
+        		}
+        	}
+        }
+	}
+}
