887f6a01f9df524c2e9ac4cd087ea3df514e4e60
junsu <junsu120202@gmail.com>
2026-04-06 14:57:33 +0900

    [Bronze IV] Title: 10!, Time: 68 ms, Memory: 11420 KB -BaekjoonHub

---
+++ 백준/Bronze/28352. 10！/10！.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        long res = 6; 
+        for(int i = 11; i <= n; i++) {
+        	res *= i;
+        }
+
+        System.out.println(res);
+	}
+}
