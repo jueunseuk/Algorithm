e7c0583d4484ca727841ee4c504e283fb3c2e19c
junsu <junsu120202@gmail.com>
2026-04-08 11:44:49 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 56 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.sh
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.sh
@@ -7,32 +7,32 @@ if [ "$n" -eq 1 ]; then
     exit 0
 fi
 
+odd=""
+even=""
+
+i=1
+while [ $i -le $n ]
+do
+    if [ $((i % 2)) -eq 1 ]; then
+        odd="${odd}*"
+        even="${even} "
+    else
+        odd="${odd} "
+        even="${even}*"
+    fi
+    i=$((i + 1))
+done
+
+result=""
 i=1
 while [ $i -le $((2 * n)) ]
 do
     if [ $((i % 2)) -eq 1 ]; then
-        j=1
-        while [ $j -le $n ]
-        do
-            if [ $((j % 2)) -eq 1 ]; then
-                printf "*"
-            else
-                printf " "
-            fi
-            j=$((j + 1))
-        done
+        result="${result}${odd}\n"
     else
-        j=1
-        while [ $j -le $n ]
-        do
-            if [ $((j % 2)) -eq 1 ]; then
-                printf " "
-            else
-                printf "*"
-            fi
-            j=$((j + 1))
-        done
+        result="${result}${even}\n"
     fi
-    printf "\n"
     i=$((i + 1))
 done
+
+printf "%b" "$result"
